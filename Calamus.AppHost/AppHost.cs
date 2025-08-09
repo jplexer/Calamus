f97b120d99f6be0da664497e79dfe63a60a0bc21
Joshua Jun <joshuajun@proton.me>
2025-08-09 19:24:37 +0200

    Remove environment variables from AppHost and add launchSettings.json for configuration
    Signed-off-by: Joshua Jun <joshuajun@proton.me>

--- Calamus.AppHost/AppHost.cs
+++ Calamus.AppHost/AppHost.cs
@@ -6,9 +6,6 @@ var pg = builder.AddPostgres("postgres")
 var calamusDb = pg.AddDatabase("calamus-db");
 
 builder.AddProject<Projects.Calamus_Bot>("bot-service")
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT",
-        Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development")
-    .WithEnvironment("DOTNET_ENVIRONMENT", Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? "Development")
     .WithReference(calamusDb).WaitFor(calamusDb);
 
 builder.Build().Run();
